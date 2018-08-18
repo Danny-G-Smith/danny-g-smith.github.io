79e6d430b025990dd875670b26e83715ac318112
Danny G Smith <dgs@campfirepixels.com>
2018-08-17 21:14:44 -0500

    removed console.log

--- js/main.js
+++ js/main.js
@@ -234,7 +234,7 @@ if ( 'serviceWorker' in navigator ) {
    window.addEventListener( 'load', () => {
       navigator.serviceWorker
          .register( '../sw_cached_site.js' )
-         .then( ( reg ) => console.log( 'Service Worker: Registered Assets' ) )
-         .catch( ( err ) => console.log( `Service Worker: Error: ${err}` ) )
+         // .then( ( reg ) => console.log( 'Service Worker: Registered Assets' ) )
+         // .catch( ( err ) => console.log( `Service Worker: Error: ${err}` ) )
    } )
 }
